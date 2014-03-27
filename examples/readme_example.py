a9ff704aea3e6589d4209ba069d52be413072b5e
zach powers <zcharlop@rockefeller.edu>
2014-03-27 16:14:50 -0400

    add a few files

--- examples/readme_example.py
+++ examples/readme_example.py
@@ -5,9 +5,11 @@ import pandas as pd
 #import some data
 #quakes = sm.datasets.get_rdataset('quakes','datasets')
 #qdf = pd.DataFrame( quakes.data )
-oldmaps = sm.datasets.get_rdataset('OldMaps','HistData')
-omdf = pd.DataFrame(oldmaps.data)
+#oldmaps = sm.datasets.get_rdataset('OldMaps','HistData')
+#omdf = pd.DataFrame(oldmaps.data)
 
+#omdf.to_csv('data/omdf.csv', index=False)
+omdf = pd.read_csv('data/omdf.csv')
 #make a map
 #PointMap(qdf).display_map()
 PointMap(omdf).display_map()
--- examples/world_population.py
+++ examples/world_population.py
@@ -3,8 +3,24 @@
 #http://dev.maxmind.com/geoip/geoip2/geolite2/
 
 import pandas as pd
-from quickD3map import MultiColumnMap
+from itertools import combinations
+from quickD3map import MultiColumnMap, LineMap
 
 df = pd.read_csv('data/city_population.csv')
 
-MultiColumnMap(df, columns = df.columns, scale_exp=8).display_map()
+#Try the MultiColumn Map, only really useful to view the populations
+#MultiColumnMap(df, columns = df.columns, scale_exp=8).display_map()
+
+#Take the Top N Cities And create a distance dataframe
+smalldf = df.sort('population_2010', ascending=False)[:15]
+
+def return_top(group):
+    return group[:1]
+    
+smalldf = smalldf.groupby('city').apply(return_top)
+print smalldf
+top_comb = combinations( list(smalldf.city) ,2)
+comb = [ [c[0],c[1],1 ] for c in top_comb ]
+distance_df = pd.DataFrame(comb)
+
+LineMap( smalldf, "city", distance_df).display_map()
